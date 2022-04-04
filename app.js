e34a8274143c2e95b25b76088ae1c7deaff4d605
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-04-04 20:12:20 -0300

    finalicé el login

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
 //server
 app.listen(PORT, ()=> console.log(
     `Servido levantado en el puerto ${PORT} 
