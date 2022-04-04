1613eca24f80dfacda03cc49055e5a417bfebd98
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-04-04 19:39:12 -0300

    finalicé el formuluario de register

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
 //server
 app.listen(PORT, ()=> console.log(
     `Servido levantado en el puerto ${PORT} 
